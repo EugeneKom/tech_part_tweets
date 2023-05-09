1638e4e45aad91bcd1fe238a690ae3cd1677fb08
EugeneKom <EugeneKomissarenko@gmail.com>
2023-05-09 23:15:19 +0200

    delet clg

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -4,7 +4,6 @@ import localStorageCustom from "../../utils/storage";
 const LoadMoreBtn = ({ incrementPage, cards }) => {
   useEffect(() => {
     localStorageCustom.save("tweets", cards);
-    console.log(cards);
   }, [cards]);
 
   return <button onClick={() => incrementPage()}>Load more</button>;
